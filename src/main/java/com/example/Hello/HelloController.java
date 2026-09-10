e54d743c2aaa4375e97ff6e39fbaa854c596807d
sakkol-git <chensakkol1124@gmail.com>
2026-09-11 00:00:45 +0700

    init 2

--- src/main/java/com/example/Hello/HelloController.java
+++ src/main/java/com/example/Hello/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello(){
-        return "Hello CI&CD pipe line + AWS";
+        return "Hello CI&CD pipe line + AWS final update";
     }
 }
